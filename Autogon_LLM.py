6e2378f16d6e82cbea8f46ed9533dfc3c6090826
whaleeu <whaleeu@gmail.com>
2024-02-17 12:46:16 +0100

    Merge LLM code

---
+++ Autogon_LLM.py
@@ -0,0 +1,37 @@
+import requests
+import json
+
+def chat_with_autogon(session_id, question, autogon_api):
+    url = "https://api.autogon.ai/api/v1/services/chatbot/e71fcd98-b129-4145-980b-1c14b2f991c5/chat/"
+
+    payload = {
+        "session_id": session_id,
+        "question": question
+    }
+
+    headers = {
+        'Content-Type': 'application/json',
+        "X-AUG-KEY": autogon_api 
+    }
+
+    response = requests.post(url, headers=headers, json=payload)
+
+    if response.status_code == 200:
+        return response.json()["data"]["bot_response"]
+    else:
+        return f"Error: {response.status_code} - {response.text}"
+
+
+
+# Example usage:
+session_id = "9a2a3752-182c-469c-bed5-b9a3d3adfa05"
+autogon_api_key = "PeD31Dan.kExR4DWsrU7sUTWSApAhbGVJCEwOVnsI"
+
+
+
+question = "list what you know?"
+
+
+
+result = chat_with_autogon(session_id, question, autogon_api_key)
+print("Chatbot response:", result)
---
+++ Image_dt.py
@@ -0,0 +1,47 @@
+from langchain_core.messages import HumanMessage
+from langchain_google_genai import ChatGoogleGenerativeAI
+import time
+
+def generate_medical_description(user_prompt, image_url):
+    GOOGLE_API_KEY = "AIzaSyCsFB_-DklkKsy1g0zBI9hCstYvfpIK0VU"
+    llm = ChatGoogleGenerativeAI(model="gemini-pro-vision", google_api_key=GOOGLE_API_KEY)
+
+    # Base prompt for the model to take the role of a doctor describing a medical image
+    base_prompt = "As a medical professional, analyze and describe the clinical findings present in the provided image. Include details such as relevant symptoms, potential diagnoses, and any notable observations. Your detailed description will assist in understanding the medical context of the image."
+
+
+    modified_user_prompt = f"{base_prompt}\n\n{user_prompt}\n\nPlease ensure that your response focuses on medical details."
+
+    max_retries = 3
+    retries = 0
+
+    while retries < max_retries:
+        try:
+            message = HumanMessage(
+                content=[
+                    {"type": "text", "text": modified_user_prompt},
+                    {"type": "image_url", "image_url": image_url},
+                ]
+            )
+
+            # Invoke the model with the user's message
+            response = llm.invoke([message])
+
+            # Return or handle the model's response
+            return response
+
+        except Exception as e:
+            print(f"Error: {e}")
+            retries += 1
+            print(f"Retrying in 5 seconds (Attempt {retries}/{max_retries})")
+            time.sleep(5)
+
+    print("Maximum retries reached. Unable to get a response.")
+    return None  
+
+
+
+user_prompt = "Please describe the symptoms or relevant details about the medical image."
+image_url = r'C:\Users\Whaleeu\Downloads\images (1).jpeg'
+result = generate_medical_description(user_prompt, image_url)
+result
